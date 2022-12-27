4b37defe9aef5c54309c527cd12e99f149753cd8
Vanderson Oliveira <98707038+Vanderson-Oliveira12@users.noreply.github.com>
2022-12-27 19:05:44 -0300

    TESTE

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -2,7 +2,7 @@ async function getDate(type, nextPagination) {
 
     /* Type: tv or movie */
 
-    let url = `https://api.themoviedb.org/3/${type}/popular/?api_key=1e049c007e395f150ceb08e474fbf7ca&language=pt-BR&page=${nextPagination}`;
+    let url = `http://api.themoviedb.org/3/${type}/popular/?api_key=1e049c007e395f150ceb08e474fbf7ca&language=pt-BR&page=${nextPagination}`;
     let response = await fetch(url);
     let data = await response.json()
 
@@ -12,7 +12,7 @@ async function getDate(type, nextPagination) {
 export default getDate;
 
 export async function getSearch(query) {
-    let url = `https://api.themoviedb.org/3/search/multi?api_key=1e049c007e395f150ceb08e474fbf7ca&language=pt-BR&query=${query}`
+    let url = `http://api.themoviedb.org/3/search/multi?api_key=1e049c007e395f150ceb08e474fbf7ca&language=pt-BR&query=${query}`
     let response = await fetch(url);
     let data = await response.json();
 
